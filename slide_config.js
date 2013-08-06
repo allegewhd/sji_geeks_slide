5e6770a2ade4ffdb71c9f6082163c550c6383cb4
Haidong Wang <wang-haidong3@sji-inc.jp>
2013-08-06 19:41:39 +0900

    init commit for sji geeks slide

---
+++ slide_config.js
@@ -0,0 +1,41 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Geek Life',
+    subtitle: 'What have I learned from GREE',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro',
+      'Ricty for Powerline'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Haidong Wang',
+    company: 'Research center for advanced technology<br>SJI',
+    gplus: 'http://plus.google.com/111417268135254875245',
+    twitter: '@allegewhd',
+    www: 'http://www.you.com',
+    github: 'http://github.com/allegewhd'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
